98a8845af0c829f49c680840df357cce1002e207
dungtophuong <dungtp@cozwork.com>
2023-12-01 12:56:01 +0700

    fix Refund store

--- app/repositories/RefundRepository.js
+++ app/repositories/RefundRepository.js
@@ -11,19 +11,43 @@ export const storeRefund = async (req) => {
     throw new Error(constants.ORDER_ALREADY_HAS_REFUND_REQUEST);
   }
   try {
-    const order = await Order.findOne({ where: { id: body.order_id } });
+    const order = await Order.findOne({
+      where: { id: body.order_id },
+      include: [
+        {
+          model: Order,
+          as: 'children',
+        },
+      ],
+    });
     if (order.parent != null) {
       throw new Error(constants.REFUND_ONLY_ALLOWED_FOR_MAIN_ORDER);
     }
-    if (req.user.id != order.customer_id || req.isSuperAdmin) {
+    if (req.user.id != order.customer_id) {
       throw new Error(constants.NOT_AUTHORIZED);
     }
     let data = body;
     data['customer_id'] = order.customer_id;
     data['amount'] = order.amount;
-    return await Refund.create(data);
+    const newRefund = await Refund.create(data);
+    if (order.children && order.children.length > 0)
+      createChildOrderRefund(order.children, data);
+    return newRefund;
   } catch (error) {
-    console.log(error);
     throw new Error(constants.NOT_FOUND);
+  }
+};
+
+const createChildOrderRefund = async (orders, data) => {
+  try {
+    orders.forEach((order) => {
+      data['order_id'] = order.id;
+      data['customer_id'] = order.customer_id;
+      data['shop_id'] = order.shop_id;
+      data['amount'] = order.amount;
+      Refund.create(data);
+    });
+  } catch (error) {
+    throw new Error(constants.SOMETHING_WENT_WRONG);
   }
 };
