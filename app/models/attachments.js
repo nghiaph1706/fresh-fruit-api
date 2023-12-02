722d194be2cf472f7c532bf061889432475b3005
nghiaph1706 <nghiaph1706@gmail.com>
2023-12-02 17:36:10 +0700

    api-attachments-index-show

--- app/models/attachments.js
+++ app/models/attachments.js
@@ -1,31 +1,40 @@
 /* jshint indent: 2 */
 
 export default function (sequelize, DataTypes) {
-  return sequelize.define('attachments', {
-    id: {
-      type: DataTypes.BIGINT,
-      allowNull: false,
-      primaryKey: true,
-      comment: 'null',
-      autoIncrement: true
+  const Attachment = sequelize.define(
+    "attachments",
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        allowNull: false,
+        primaryKey: true,
+        comment: "null",
+        autoIncrement: true,
+      },
+      url: {
+        type: DataTypes.STRING(191),
+        allowNull: false,
+        defaultValue: "",
+        comment: "null",
+      },
+      created_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        comment: "null",
+      },
+      updated_at: {
+        type: DataTypes.DATE,
+        allowNull: true,
+        comment: "null",
+      },
     },
-    url: {
-      type: DataTypes.STRING(191),
-      allowNull: false,
-      defaultValue: '',
-      comment: 'null'
-    },
-    created_at: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      comment: 'null'
-    },
-    updated_at: {
-      type: DataTypes.DATE,
-      allowNull: true,
-      comment: 'null'
+    {
+      tableName: "attachments",
+      timestamps: true,
+      createdAt: "created_at",
+      updatedAt: "updated_at",
     }
-  }, {
-    tableName: 'attachments'
-  })
-};
+  );
+
+  return Attachment;
+}
