b96d2445812a365e32f82a8835136e047b6df154
phuocvm <vuuminhphuoc@gmail.com>
2023-12-03 23:29:24 +0700

    remove crypto

--- app/repositories/BaseRepository.js
+++ app/repositories/BaseRepository.js
@@ -1,36 +1,15 @@
-// public function makeSlug(Request $request, string $key = '', ?int $update = null): string
-// {
-//     $slugText = match (true) {
-//         !empty($request->slug)  => $request->slug,
-//         !empty($request->name)  => $request->name,
-//         !empty($request->title) => $request->title,
-//         !empty($request[$key])  => $request[$key],
-//         empty($request->slug)   => 'auto-generated-string',
-//     };
-//     if (empty($key)) {
-//         return globalSlugify(slugText: $slugText, model: $this->model(), update: $update);
-//     }
-//     return globalSlugify(slugText: $request[$key], model: $this->model(), key: $key, update: $update);
-// }
-import { globalSlugify } from "../helpers/helpers.js";
+import { customSlugify } from "../services/UtilServcie.js";
 export const makeSlug = (req, key = "", update = null) => {
   const slugText = (() => {
-    if (req.slug) {
-      return req.slug;
-    } else if (req.name) {
-      return req.name;
-    } else if (req.title) {
-      return req.title;
-    } else if (req[key]) {
-      return req[key];
+    if (req.body.slug) {
+      return req.body.slug;
+    } else if (req.body.name) {
+      return req.body.name;
+    } else if (req.body.title) {
+      return req.body.title;
     } else {
       return "auto-generated-string";
     }
   })();
-
-  if (key === "") {
-    return globalSlugify(slugText, update);
-  } else {
-    return globalSlugify(req[key], update);
-  }
+  return customSlugify(slugText);
 };
